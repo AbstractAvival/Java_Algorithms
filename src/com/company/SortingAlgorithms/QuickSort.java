3431a88c41fa09f0471018862a5ff0f52a4d81b6
AbstractAvival <96794792+AbstractAvival@users.noreply.github.com>
2022-05-05 16:47:01 -0500

    Implemented Sort method.

--- src/com/company/SortingAlgorithms/QuickSort.java
+++ src/com/company/SortingAlgorithms/QuickSort.java
@@ -2,10 +2,15 @@ package com.company.SortingAlgorithms;
 
 public class QuickSort {
     public void Sort( int [] unsortedArray, int startIndex, int endIndex ) {
+        if( startIndex >= endIndex )
+            return;
 
+        int pivotIndex = Partition( unsortedArray, startIndex, endIndex );
+        Sort( unsortedArray, startIndex, pivotIndex - 1 );
+        Sort( unsortedArray, pivotIndex + 1, endIndex );
     }
 
-    private int Partition() {
+    private int Partition( int [] unsortedArray, int startIndex, int endIndex ) {
         return 0;
     }
 }
